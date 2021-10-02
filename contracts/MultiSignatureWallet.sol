36c3c82791c8592a58cac774509cbd9f6a0bbd9c
Ben Naylor <36206067+bnaylor1989@users.noreply.github.com>
2021-10-02 12:29:29 +0100

    Completed exercise

--- contracts/MultiSignatureWallet.sol
+++ contracts/MultiSignatureWallet.sol
@@ -2,6 +2,20 @@ pragma solidity ^0.5.0;
 
 contract MultiSignatureWallet {
 
+    event Deposit(address indexed sender, uint value); // indexed = more searchable
+    event Submission(uint indexed transactionId);
+    event Confirmation(address indexed sender, uint indexed transactionId);
+    event Execution(uint indexed transactionId);
+    event ExecutionFailure(uint indexed transactionId);
+
+    address[] public owners;
+    uint public required;
+    uint public transactionCount;
+    
+    mapping (address => bool) public isOwner;
+    mapping (uint => Transaction) public transactions;
+    mapping (uint => mapping (address => bool)) public confirmations;
+   
     struct Transaction {
       bool executed;
       address destination;
@@ -9,7 +23,12 @@ contract MultiSignatureWallet {
       bytes data;
     }
 
-    event Deposit(address indexed sender, uint value);
+
+    modifier validRequirement(uint ownerCount, uint _required) {
+        if (_required > ownerCount || _required == 0 || ownerCount == 0)
+        revert();
+        _;
+    }
 
     /// @dev Fallback function allows to deposit ether.
     function()
@@ -27,18 +46,43 @@ contract MultiSignatureWallet {
     /// @dev Contract constructor sets initial owners and required number of confirmations.
     /// @param _owners List of initial owners.
     /// @param _required Number of required confirmations.
-    constructor(address[] memory _owners, uint _required) public {}
+    constructor(address[] memory _owners, uint _required) public validRequirement(_owners.length, _required){
+        for (uint i=0; i<_owners.length; i++){
+            isOwner[_owners[i]] = true;
+        }
+        owners = _owners;
+        required = _required;
+    }
+
+
 
     /// @dev Allows an owner to submit and confirm a transaction.
     /// @param destination Transaction target address.
     /// @param value Transaction ether value.
     /// @param data Transaction data payload.
     /// @return Returns transaction ID.
-    function submitTransaction(address destination, uint value, bytes memory data) public returns (uint transactionId) {}
+    function submitTransaction(address destination, uint value, bytes memory data) public returns (uint transactionId) {
+        require(isOwner[msg.sender]);
+
+        // add transaction
+        transactionId = addTransaction(destination, value, data);
+        // confirm transaction
+        confirmTransaction(transactionId);
+
+
+    }
 
     /// @dev Allows an owner to confirm a transaction.
     /// @param transactionId Transaction ID.
-    function confirmTransaction(uint transactionId) public {}
+    function confirmTransaction(uint transactionId) public {
+        require(isOwner[msg.sender]);
+        // Below: check that a transaction exists at the specified transactionId
+        require(transactions[transactionId].destination != address(0));
+        require(confirmations[transactionId][msg.sender] == false);
+        confirmations[transactionId][msg.sender] = true;
+        emit Confirmation(msg.sender, transactionId);
+        executeTransaction(transactionId);
+    }
 
     /// @dev Allows an owner to revoke a confirmation for a transaction.
     /// @param transactionId Transaction ID.
@@ -46,7 +90,23 @@ contract MultiSignatureWallet {
 
     /// @dev Allows anyone to execute a confirmed transaction.
     /// @param transactionId Transaction ID.
-    function executeTransaction(uint transactionId) public {}
+    function executeTransaction(uint transactionId) public {
+        require(transactions[transactionId].executed == false);
+        
+        if(isConfirmed(transactionId)) {
+            // using the "storage" keyword makes "t" a pointer to storage 
+            Transaction storage t = transactions[transactionId];
+            t.executed = true;
+            (bool success, bytes memory returnedData) = t.destination.call.value(t.value)(t.data);
+            if (success)
+                emit Execution(transactionId);
+            else {
+                emit ExecutionFailure(transactionId);
+                t.executed = false;
+            }
+        }
+
+    }
 
 		/*
 		 * (Possible) Helper Functions
@@ -54,12 +114,33 @@ contract MultiSignatureWallet {
     /// @dev Returns the confirmation status of a transaction.
     /// @param transactionId Transaction ID.
     /// @return Confirmation status.
-    function isConfirmed(uint transactionId) internal view returns (bool) {}
+    function isConfirmed(uint transactionId) internal view returns (bool) {
+        // verfiy that the tansaction has the required number of confirmations
+        // loop over the owners array and count how many of the owners have confirmed the transaction.
+        uint count = 0;
+        for(uint i=0; i< owners.length; i++){
+            if (confirmations[transactionId][owners[i]])
+                count+=1;
+            
+            if (count == required) return true;
+        }
+    }
 
     /// @dev Adds a new transaction to the transaction mapping, if transaction does not exist yet.
     /// @param destination Transaction target address.
     /// @param value Transaction ether value.
     /// @param data Transaction data payload.
     /// @return Returns transaction ID.
-    function addTransaction(address destination, uint value, bytes memory data) internal returns (uint transactionId) {}
+    function addTransaction(address destination, uint value, bytes memory data) internal returns (uint transactionId) {
+        transactionId = transactionCount;
+        transactions[transactionId] = Transaction({
+            destination: destination,
+            value: value,
+            data: data,
+            executed: false
+        });
+        transactionCount += 1;
+        emit Submission(transactionId);
+
+    }
 }
